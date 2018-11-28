aff61baac15cad886d3c5696df00585d35376171
camel_ys <camel_yangz@163.com>
2018-11-28 16:17:00 +0800

    自己用的一些工具

---
+++ js/droptoggle.js
@@ -0,0 +1,11 @@
+$('.btn-group').on('click', 'button', function (e) {
+    if(this.dataset.toggle === 'dropdown'){
+        $(this).next().toggle();
+        $('.layui-laydate').hide();
+        e.stopPropagation();
+    }
+});
+
+document.body.addEventListener('click', function (e) {
+    $('.dropdown-menu').hide();
+});
